573ec217128f8aa1212bdc0ff2b957c18c3afb76
Dannysnotepad <etimdnl41@gmail.com>
2023-09-21 02:35:50 +0100

    🌟v0.5

--- main.js
+++ main.js
@@ -1,60 +1,66 @@
 /*
-*WhatsApp links generator
-*from
-*Dannysnotepad
-*/
+ *WhatsApp links generator
+ *from
+ *Dannysnotepad
+ */
 
 const input = document.getElementById('input')
 const generateBtn = document.getElementById('generate')
 const emptyState = document.querySelector('emptyState')
 let ulEl = document.querySelector('ul')
 let linkTemplate = `https://wa.me/`
-let linkArray = []
-let liText = ''
+let linkArray = localStorage.getItem('linkArray') ? JSON.parse(localStorage.getItem('linkArray')) : [];
+renderLinks();
 
 
-function copy(){
+
+
+function copy(e) {
   alert('>_< This feature is still under development')
-  
+  //navigator.clipboard.writeText(linkArray.linkTemplate)
 }
 
-function delet(e){
+function delet(e) {
   e.parentElement.remove();
-  linkArray.splice(e.parentElement.id, 0);
+  linkArray.splice(e.parentElement.id, 1);
+  localStorage.setItem("linkArray", JSON.stringify(linkArray));
 }
 
-function createLinks(){
+function renderLinks() {
+  let liText = ''
+  for (const link of linkArray) {
+    liText += `
+        <li data-key="${link.id}"><a href="${link.linkTemplate}">${link.linkTemplate}</a> <span class="material-symbols-outlined hi" onclick="copy()">
+                    content_copy
+                    </span><span class="material-symbols-outlined" onclick="delet(this)">
+                    delete
+                </span></li>
+        `
+  }
+  ulEl.innerHTML += liText
+}
+
+function createLinks() {
   if (input.value == '' || input.value == null || isNaN(input.value)) {
-    
     input.focus()
     return false
   }
+
   let value = input.value
   linkTemplate = `https://wa.me/${value}`
   let linkArrayContent = {
     linkTemplate,
     id: Date.now()
   }
+
   linkArray.push(linkArrayContent)
-  
-  
-  
-  for (let i = 0; i < linkArray.length; i++){
-    liText = `
-    <li data-key="${linkArrayContent.id}"><a href="${linkArrayContent.linkTemplate}">${linkArrayContent.linkTemplate}</a> <span class="material-symbols-outlined hi" onclick="copy()">
-              content_copy
-            </span><span class="material-symbols-outlined" onclick="delet(this)">
-              delete
-            </span></li>
-    `
-  }
-  const item = document.querySelector(`[data-key='${linkArrayContent.id}']`);
-  ulEl.innerHTML += liText
-  
+  localStorage.setItem('linkArray', JSON.stringify(linkArray))
+  renderLinks()
 }
 
-
 generate.addEventListener('click', () => {
+  location.replace(location.href)
   createLinks()
   input.value = ''
+  
 })
